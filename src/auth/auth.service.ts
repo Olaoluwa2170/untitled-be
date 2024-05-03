f1df0f9e100183d565ba65c57c2a02a475fccc8f
Babalola Elisha <elitebabson@gmail.com>
2024-05-03 20:59:04 +0100

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,53 +6,59 @@ import { compare, hash } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-    constructor(private prismaService: PrismaService, private jwtService: JwtService) {}
+  async signup(dto: AuthSignUpDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        username: dto.username,
+      },
+    });
 
-    async signup(dto: AuthSignUpDto) {
-        const user = await this.prismaService.user.findUnique({
-            where: {
-                username: dto.username
-            }
-        })
-        
-        if(user) throw new HttpException("User Already Exists!", HttpStatus.CONFLICT)
+    if (user)
+      throw new HttpException('User Already Exists!', HttpStatus.CONFLICT);
 
-        const hashedPassword = await hash(dto.password, 10)
-        
-        const newUser = await this.prismaService.user.create({
-            data: {
-                username: dto.username,
-                password: hashedPassword,
-                email: dto.email,
-                role: dto.role
-            }
-        })
-        
-        const access_token = this.jwtService.sign(newUser)
-        
-        return access_token
-    }
+    const hashedPassword = await hash(dto.password, 10);
 
-    async login(dto: AuthLoginDto) {
-        const user = await this.prismaService.user.findUnique({
-            where: {
-                username: dto.username
-            }
-        })
-        
-        if(!user) throw new HttpException("User does not exist", HttpStatus.UNAUTHORIZED)
+    const newUser = await this.prismaService.user.create({
+      data: {
+        username: dto.username,
+        password: hashedPassword,
+        email: dto.email,
+        role: dto.role,
+      },
+    });
 
-        const isPasswordCorrect = await compare(dto.password, user.password)
-        
-        if(!isPasswordCorrect) throw new HttpException("User Unauthorized", HttpStatus.UNAUTHORIZED)
-        const {password, ...data} = user
-        const access_token = this.jwtService.sign(data)
-        
-        return access_token
-    }
+    const access_token = this.jwtService.sign(newUser);
 
-    logout() {
-        return ["benji"]
-    }
+    return access_token;
+  }
+
+  async login(dto: AuthLoginDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        username: dto.username,
+      },
+    });
+
+    if (!user)
+      throw new HttpException('User does not exist', HttpStatus.UNAUTHORIZED);
+
+    const isPasswordCorrect = await compare(dto.password, user.password);
+
+    if (!isPasswordCorrect)
+      throw new HttpException('User Unauthorized', HttpStatus.UNAUTHORIZED);
+    const { ...data } = user;
+    // const { password, ...data } = user;
+    const access_token = this.jwtService.sign(data);
+
+    return access_token;
+  }
+
+  logout() {
+    return ['....'];
+  }
 }
