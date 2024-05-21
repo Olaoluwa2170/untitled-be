83274700241deb7fb9bf87d2f63c34c83b564d71
Babalola Elisha <elitebabson@gmail.com>
2024-05-21 15:21:54 +0100

    added cookies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Get, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
