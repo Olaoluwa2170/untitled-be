8b5edf7926ee4a6d886110211c5364d32639588c
Babalola Elisha <elitebabson@gmail.com>
2024-06-05 22:25:49 +0100

    thirst

--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -4,6 +4,7 @@ const allowedOrigins = [
   'http://localhost:3100',
   'http://localhost:5174',
   'http://localhost:5173',
+  'https://untitled-be.onrender.com',
 ];
 
 export default allowedOrigins;
