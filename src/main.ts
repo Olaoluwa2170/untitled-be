be1f88989668f4d8aec16c4b900a64609d8ff01e
Babalola Elisha <elitebabson@gmail.com>
2024-06-05 23:18:55 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
-import allowedOrigins from './config/allowedOrigin';
+// import allowedOrigins from './config/allowedOrigin';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,11 +10,12 @@ dotenv.config();
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: allowedOrigins,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
+  // app.enableCors({
+  //   origin: allowedOrigins,
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
   app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
