904d6e92169d2a462ebe8f4fd21a18e6c8c336b9
Babalola Elisha <elitebabson@gmail.com>
2024-06-06 08:13:53 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,31 @@ import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 // import allowedOrigins from './config/allowedOrigin';
 import * as dotenv from 'dotenv';
+import { Response, Request, NextFunction } from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://your-frontend.com');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers',
+    );
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Private-Network', 'true');
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader('Access-Control-Max-Age', 7200);
+
+    next();
+  });
+  // app.enableCors();
   // app.enableCors({
   //   origin: allowedOrigins,
   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
