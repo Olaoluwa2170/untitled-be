f932a13224be360ee1b312fef3cd6ce3c1ebf146
Babalola Elisha <elitebabson@gmail.com>
2024-06-08 11:37:13 +0100

    chore: solved cors problem

--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -1,7 +1,6 @@
 const allowedOrigins = [
-  'http://localhost:3000',
   'http://localhost:5173',
-  'https://untitled-be.onrender.com',
+  'https://untitiled-fe.vercel.app',
 ];
 
 export default allowedOrigins;
