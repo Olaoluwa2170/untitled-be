4466a8fc6f0422585c6044b9affc817306eac271
Babalola Elisha <elitebabson@gmail.com>
2024-06-17 15:35:25 +0100

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,31 +20,3 @@ import { EventsModule } from './events/events.module';
   ],
 })
 export class AppModule {}
-
-// import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
-// import { PrismaModule } from './prisma/prisma.module';
-// import { TicketsModule } from './tickets/tickets.module';
-// import { APP_GUARD } from '@nestjs/core';
-// import { AtGuard } from './auth/guards/at.guard';
-// import { EventsModule } from './events/events.module';
-// import { CorsMiddleware } from './cors-middleware/cors-middleware.middleware';
-
-// @Module({
-//   imports: [AuthModule, PrismaModule, TicketsModule, EventsModule],
-//   controllers: [AppController],
-//   providers: [
-//     {
-//       provide: APP_GUARD,
-//       useClass: AtGuard,
-//     },
-//     AppService,
-//   ],
-// })
-// export class AppModule implements NestModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer.apply(CorsMiddleware).forRoutes('*'); // Apply the middleware to all routes
-//   }
-// }
