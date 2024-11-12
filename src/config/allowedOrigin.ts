cf1f541850fc0d00525e8f38376fb3f1de7a1fef
Olaoluwa2170 <elitebabson@gmail.com>
2024-11-12 19:11:37 +0100

    changed allowed cors to *

--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -1,8 +1,10 @@
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://untitled-be.vercel.app/',
-  'https://untitiled-fe.vercel.app',
-  // 'https://untitled-fe-mauve.vercel.app',
-];
+const allowedOrigins = '*';
 
 export default allowedOrigins;
+
+// [
+//   'http://localhost:5173',
+//   'https://untitled-be.vercel.app/',
+//   'https://untitiled-fe.vercel.app',
+//   'https://untitled-fe-mauve.vercel.app',
+// ];
